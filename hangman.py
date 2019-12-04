5c5024676157f508e62c5895867e316b0e08c0d8
Jay-Goss <42222447+Jay-Goss@users.noreply.github.com>
2019-12-04 11:34:28 -0500

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -2,40 +2,46 @@
 
 import random
 
-word = random.choice(["paladin", "warlock", "shaman", "mage", "warrior", "priest"])
-print(word)
-print(len(word))
+def createWord():
+    createWord.word = random.choice(["paladin", "warlock", "shaman", "mage", "warrior", "priest", "rogue", "raganaros"]).upper()
+    # print(createWord.word)
+    # print(len(createWord.word))
 
 def check(guess, guesses, word):
     displayWord = ''
     i = 0
     matches = 0
-    for letter in word:
+    for letter in createWord.word:
         if letter in guesses:
             displayWord += letter
             matches += 1
         else:
             displayWord += "@"
-    print(displayWord)
-
-guesses = []
-
+    print(displayWord.upper())
+    if guess in createWord.word:
+        print("yes good guess")
+    elif guess not in createWord.word:
+        print("no bad guess")
+    if matches == len(word):
+        print("that's it you won! Time for a new game!")
+        playGame()
+        
 def playGame():
+    createWord()
+    guesses = []
     solved = False
-    while not solved:
-        print("The word has " + str(len(word)) + " letters")
-        guess = input("Guess a letter!")
+    print("The secret word has " + str(len(createWord.word)) + " letters.")
+    while not solved:        
+        guess = input("Guess a letter!").upper()
         if len(guess) != 1:
-            print("guess one letter at a time scrub")
-            playGame()
+            print("guess one at a time scrub")
         elif guess in guesses:
-            print("you already guessed that dumdum")
-            playGame()
+            print("you already guessed that dumdum! here are your guesses to remind you.")
+            print(guesses)
+        elif not str.isalpha(guess):
+            print("guess a letter ya turd")
         else:
             guesses.append(guess)
-            result = check(guess, guesses, word)
-            print(result)
-        # print(guess)
-        # print(guesses)
-
+            check(guess, guesses, createWord.word)
+            
 playGame()
